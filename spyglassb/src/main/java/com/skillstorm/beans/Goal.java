e5735115fb8f81f7e363a7b0b060258b78f9a9ba
Xhuljano Racaku <racakuxhuljano@yahoo.com>
2022-08-29 10:32:55 -0400

    Added Goal Bean
    Created the folders and packages for controller, service, bean, and repository. Also goal bean is done

---
+++ spyglassb/src/main/java/com/skillstorm/beans/Goal.java
@@ -0,0 +1,122 @@
+package com.skillstorm.beans;
+
+import java.time.LocalDate;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "goals")
+public class Goal {
+	@Id
+	@Column(name= "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name= "name")
+	private String name;
+	
+	@Column(name= "description")
+	private String description;
+	
+	@Column(name= "image")
+	private String image;
+	
+	@Column(name= "target_date")
+	private LocalDate targetDate;
+	
+	@Column(name= "target_amount")
+	private double targetAmount;
+	
+	@Column(name= "saved_amount")
+	private double savedAmount;
+
+	public Goal() {
+		
+	}
+
+	public Goal(String name, String description, String image, LocalDate targetDate, double targetAmount,
+			double savedAmount) {
+		this.name = name;
+		this.description = description;
+		this.image = image;
+		this.targetDate = targetDate;
+		this.targetAmount = targetAmount;
+		this.savedAmount = savedAmount;
+	}
+
+	public Goal(int id, String name, String description, String image, LocalDate targetDate, double targetAmount,
+			double savedAmount) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.image = image;
+		this.targetDate = targetDate;
+		this.targetAmount = targetAmount;
+		this.savedAmount = savedAmount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public LocalDate getTargetDate() {
+		return targetDate;
+	}
+
+	public void setTargetDate(LocalDate targetDate) {
+		this.targetDate = targetDate;
+	}
+
+	public double getTargetAmount() {
+		return targetAmount;
+	}
+
+	public void setTargetAmount(double targetAmount) {
+		this.targetAmount = targetAmount;
+	}
+
+	public double getSavedAmount() {
+		return savedAmount;
+	}
+
+	public void setSavedAmount(double savedAmount) {
+		this.savedAmount = savedAmount;
+	}
+
+	@Override
+	public String toString() {
+		return "Goal [id=" + id + ", name=" + name + ", description=" + description + ", image=" + image
+				+ ", targetDate=" + targetDate + ", targetAmount=" + targetAmount + ", savedAmount=" + savedAmount
+				+ "]";
+	}
+	
+	
+}
---
+++ spyglassb/src/main/java/com/skillstorm/repository/GoalRepository.java
@@ -0,0 +1,5 @@
+package com.skillstorm.repository;
+
+public interface GoalRepository  {
+
+}
